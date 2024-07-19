4d36a3959cc604c4e8501f36aa25f17b62cabfff
samrudhmoode <samrudhmoode@gmail.com>
2024-07-18 17:34:58 -0700

    working multi ptp spline starting and stopping at each target

--- src/moveit_motion/moveit_motion/combining_waypoint.py
+++ src/moveit_motion/moveit_motion/combining_waypoint.py
@@ -44,6 +44,10 @@ def main():
         Pose(
                 position=Point(x=0.5, y=0.1, z=0.4),
                 orientation=Quaternion(x=0.0, y=-1.0, z=0.0, w=0.0),
+            ),
+        Pose(
+                position=Point(x=0.0, y=0.0, z=1.266),
+                orientation=Quaternion(x=0.0, y=0.0, z=0.0, w=1.0),
             )
     ]
 
