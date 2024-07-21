072c42fe0c24de4a04a56438093d5c0d3f65d728
rysabh <rishabhshukla2000@gmail.com>
2024-07-21 16:51:47 -0700

    2024-07-21 16:51:47

--- src/moveit_motion/moveit_motion/ros_submodules/rviz_link.py
+++ src/moveit_motion/moveit_motion/ros_submodules/rviz_link.py
@@ -1,25 +1,29 @@
 import rclpy
 from rclpy.node import Node
 from tf2_ros import TransformBroadcaster
-from geometry_msgs.msg import TransformStamped, Quaternion, Vector3
+from geometry_msgs.msg import TransformStamped, Quaternion, Vector3, Point
 from std_msgs.msg import ColorRGBA
 from visualization_msgs.msg import Marker
 from tf2_ros import TransformBroadcaster
 from nav_msgs.msg import Path
-
+import numpy as np
 class RvizLink(Node):
     def __init__(self):
         super().__init__('rviz_link')
         self.br = TransformBroadcaster(self)
         self.index = 0
-        self.callback_function = None
+        self.callback_functions = []
 
     
     def start_simulation(self, interval: float = 1/10) -> None:
-        if self.callback_function is None:
-            raise ValueError("Callback function is not defined.")
+        if not self.callback_functions:
+            raise ValueError("Callback functions are not defined.")
         
-        self.timer = self.create_timer(interval, self.callback_function)
+        # merge all callback functions into one
+        def _major_callback_function():
+            for callback in self.callback_functions:
+                callback()
+        self.timer = self.create_timer(interval, _major_callback_function)
 
     def pub_marker(self, namespace, frame_id, translation, rotation, 
                     marker_id=0, marker_type=Marker.CUBE, scale=(0.3, 0.3, 0.3), 
@@ -31,28 +35,35 @@ class RvizLink(Node):
         marker.id = marker_id
         marker.type = marker_type
         marker.action = Marker.ADD
-        marker.pose.position = Vector3(x=translation[0], y=translation[1], z=translation[2])
-        marker.pose.orientation = Quaternion(x=rotation[0], y=rotation[1], z=rotation[2], w=rotation[3])
-        marker.scale = Vector3(x=scale[0], y=scale[1], z=scale[2])
-        marker.color = ColorRGBA(r=color[0], g=color[1], b=color[2], a=color[3])
-        self.get_logger().info(f'Publishing marker: {marker}')
+        
+        marker.pose.position = Point(x=float(translation[0]), y=float(translation[1]), z=float(translation[2]))
+        marker.pose.orientation = Quaternion(x=float(rotation[0]), y=float(rotation[1]), z=float(rotation[2]), w=float(rotation[3]))
+        marker.scale = Vector3(x=float(scale[0]), y=float(scale[1]), z=float(scale[2]))
+        marker.color = ColorRGBA(r=float(color[0]), g=float(color[1]), b=float(color[2]), a=float(color[3]))
         self.marker_pub.publish(marker)
+
     
     def pub_markers(self, markers: list[list], frame_id: str, 
                     marker_type=Marker.SPHERE, scale=(0.01,0.01,0.01), color=(1.0, 0.0, 0.0, 1.0)) -> None:
         
-        for index, TxyzQwxyz in enumerate(markers):
+        for _index, TxyzQwxyz in enumerate(markers):
             Txyz = TxyzQwxyz[0:3]
-            Qxyzw = [0.0, 0.0, 0.0, 1.0]
+            Qxyzw = np.array([0.0, 0.0, 0.0, 1.0])
             if len(TxyzQwxyz) == 7:
                 Qxyzw = TxyzQwxyz[3:]
-                
-            
-        def _callback_function(self):
-            self.pub_marker(frame_id = frame_id, namespace='m', translation=Txyz, rotation=Qxyzw,
-                            marker_id=index, marker_type=marker_type, scale=scale, color=color)
+            markers[_index] = np.concatenate((Txyz, Qxyzw))
         
-        self.callback_function = _callback_function
+        self.markers = markers
+        
+        def _callback_function():
+            self.pub_marker(frame_id = frame_id, namespace=f'm{self.index}', translation=self.markers[self.index][:3], rotation=self.markers[self.index][3:],
+                            marker_id=self.index, marker_type=marker_type, scale=scale, color=color)
+            
+            self.get_logger().info(f'Publishing markers: {self.index}'); self.index += 1
+        
+        self.get_logger().info(f'creating callback function for markers')
+        self.callback_functions.append(_callback_function)
+        
 
     # def pub_path(self, frame_id: str, path: list[list]) -> None:
     #     path_msg = Path()
@@ -94,14 +105,26 @@ class RvizLink(Node):
         _t.transform.rotation.w = rotation[3]
         self.br.sendTransform(_t)
 
-    def pub_robot(self, parent_frame:str, base_link: str, links: list, translations: list, rotations: list) -> None:
+    def pub_robot(self, parent_frame:str, base_link: str, translation: list, rotation: list) -> None:
         _t = TransformStamped()
         _t.header.stamp = self.get_clock().now().to_msg()
         _t.header.frame_id = parent_frame
         _t.child_frame_id = base_link
-        _t.transform.translation = Vector3(x=-0.5, y=-0.5, z=0.009)
-        _t.transform.rotation = Quaternion(x=0.0, y=0.0, z=0.0, w=1.0)
-        self.br.sendTransform(_t)
+        _t.transform.translation = Vector3(x=translation[0], y=translation[1], z=translation[2])
+        _t.transform.rotation = Quaternion(x=rotation[0], y=rotation[1], z=rotation[2], w=rotation[3])
+        
+        if not hasattr(self, 'robots'):
+            self.robots = []
+
+        self.robots.append(_t)
+
+        def _callback_function():
+            for robot in self.robots:
+                self.br.sendTransform(robot)
+
+        self.get_logger().info(f'creating callback function for Robots')
+        self.callback_functions.append(_callback_function)
+
 
     def create_path_publisher(self, topic: str = "path") -> None:
         self.path_pub = self.create_publisher(Path, topic, 10)
--- src/moveit_motion/moveit_motion/viz_markers.py
+++ src/moveit_motion/moveit_motion/viz_markers.py
@@ -24,17 +24,24 @@ def main():
 
     file_path = "src/moveit_motion/moveit_motion/diffusion_policy_cam/diffusion_pipline/data_chisel_task/cleaned_traj/cap_010_cleaned.csv"    
 
-    data = cfp.DataParser.from_quat_file(file_path = file_path, target_fps= 10.0, filter=True, window_size=15, polyorder=3)
+    data = cfp.DataParser.from_quat_file(file_path = file_path, target_fps= 120.0, filter=True, window_size=15, polyorder=3)
 
     RL = RvizLink()
-
+    RL.pub_robot(parent_frame='world', base_link='kuka_green_link_0', translation=(0.5, 0.5, 0.009), rotation=(0.0, 0.0, 0.0, 1.0))
+ 
+    RL.create_marker_publisher()
+    print()
+    # RL.pub_marker(namespace='m', frame_id='world', translation=[0,0,0], rotation=[0,0,0,1], scale=(0.3, 0.3, 0.3), color=(0.0, 1.0, 0.0, 1.0))
+    # print()
     data_chisel = data.get_rigid_TxyzQwxyz()["chisel"]
-    data_times = data.get_time()
+    # data_times = data.get_time()
 
     data_chisel = np.apply_along_axis(ros_m.robodk_2_ros, 1, data_chisel)
 
     RL.pub_markers(markers=data_chisel, frame_id='world')
 
+    RL.start_simulation(interval=1/10)
+    rclpy.spin(RL)
 
 if __name__ == "__main__":
     main()
