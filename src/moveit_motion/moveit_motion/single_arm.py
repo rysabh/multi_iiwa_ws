0085a5ceef34fcb2bcdb9a8ef899901545ce28b0
samrudhmoode <samrudhmoode@gmail.com>
2024-07-24 18:02:42 -0700

    Wrote single arm, requires testing

--- src/moveit_motion/moveit_motion/single_arm.py
+++ src/moveit_motion/moveit_motion/single_arm.py
@@ -7,6 +7,42 @@ from sensor_msgs.msg import JointState
 from ros_submodules.RS_submodules import save_trajectory, save_trajectory_to_csv, MSE_joint_states
 
 from ros_submodules.MoveitInterface import MoveitInterface
+from control_msgs.action import FollowJointTrajectory
+from ros_submodules.wait_for_message import wait_for_message
+
+class FollowJointAction(Node):
+    def __init__(self, node: str, move_group_name:str = "arm"):
+        super().__init__(node)
+
+        self.follow_joint_traj = f"/{move_group_name}/joint_trajectory_controller/follow_joint_trajectory"
+        self.trajectory_client = ActionClient(self, FollowJointTrajectory, self.follow_joint_traj)
+        if not self.trajectory_client.wait_for_server(timeout_sec=1):
+            raise RuntimeError(f"Couldn't connect to Action Server {self.follow_joint_traj}.")
+
+    def execute_traj_on_robot(self, joint_traj):
+        goal = FollowJointTrajectory.Goal()
+        goal.trajectory = joint_traj
+        goal_future = self.trajectory_client.send_goal_async(goal)
+        rclpy.spin_until_future_complete(self, goal_future)
+        goal_handle = goal_future.result()
+        if not goal_handle.accepted:
+            self.get_logger().error("Trajectory goal rejected")
+            return
+
+        # Wait for result
+        self.get_logger().info("Trajectory goal accepted")
+        self.get_logger().info("Waiting for result...")
+        result_future = goal_handle.get_result_async()
+        rclpy.spin_until_future_complete(self, result_future)
+
+    def get_current_robot_joint_state(self):
+        _MSG_RECEIVED_BOOL, current_joint_state = wait_for_message(
+            JointState, self, f"/{self.move_group_name}/joint_states", time_to_wait=3.0
+        )
+        if not _MSG_RECEIVED_BOOL:
+            self.get_logger().error("Failed to get current joint state")
+            return None
+        return current_joint_state
 
 def main():
     rclpy.init()
@@ -15,6 +51,9 @@ def main():
                                   remapping_name="",           # lbr # ""          #-> required for service and action remapping
                                   prefix="kuka_green",          # ""  # kuka_g/b..   #-> required for filtering joint states and links
                                  )
+    follow_traj_client = FollowJointAction(node="follow_traj",
+                                           move_group_name="kuka_green")
+
     
     # client = MoveitInterface(node_name="client",     
     #                               move_group_name="arm", 
@@ -35,7 +74,14 @@ def main():
             )
     ]
 
-    dual_spline_trajectory = []
+    robot_state = follow_traj_client.get_current_robot_joint_state()
+    cjs = client.get_current_robot_pose()
+    plan = client.get_joint_traj(target_joint_state=robot_state, 
+                                                  start_joint_state=cjs,
+                                                  planner_type="ompl")
+    client.execute_joint_traj(fixed_trajectory)
+
+    total_trajectory = []
     
     cjs = client.get_current_joint_state()
     print(cjs)
@@ -47,12 +93,16 @@ def main():
         print(tjs)
         plan = client.get_joint_traj(target_joint_state=tjs, 
                                                   start_joint_state=cjs,
-                                                  planner_type="pilz")
-        
+                                                  planner_type="ompl")
+        total_trajectory.append(plan)
         cjs = tjs
     
     # combined_trajectory = client_dual.combine_trajectories(dual_spline_trajectory)
-
+    fixed_trajectory = client.combine_trajectories(total_trajectory)
+    client.execute_joint_traj(fixed_trajectory)
+    execution = input("Do you want to execute? y/n")
+    if execution.lower() == "y":
+        follow_traj_client.execute_traj_on_robot(fixed_trajectory)
     # client_dual.execute_joint_traj(combined_trajectory)
     
 
