1e16777513d3de97d2e11b1d37fc4e692c4bc4b9
samrudhmoode <samrudhmoode@gmail.com>
2024-08-22 15:21:36 -0700

    moveit launch file works

--- src/python_sequence_planner/python_sequence_planner/python_sequence_planner.py
+++ src/python_sequence_planner/python_sequence_planner/python_sequence_planner.py
@@ -19,7 +19,7 @@ from std_msgs.msg import Header
 class SequenceMotion(Node):
     def __init__(self):
         super().__init__("sequence_motion")
-        self._action_client = ActionClient(self, MoveGroupSequence, "/kuka_green/sequence_move_group")
+        self._action_client = ActionClient(self, MoveGroupSequence, "/sequence_move_group")
 
         while not self._action_client.wait_for_server(timeout_sec=1.0):
             self.get_logger().info(f"Waiting for {self._action_client._action_name}...")
@@ -42,7 +42,7 @@ class SequenceMotion(Node):
             Constraints(
                 position_constraints=[
                     PositionConstraint(
-                        header = Header(frame_id = self.base),
+                        header = Header(frame_id = self._base),
                         link_name = self._end_effector,
                         constraint_region = BoundingVolume(
                             primitives = [SolidPrimitive(type=2, dimensions=[0.0001])],
@@ -53,7 +53,7 @@ class SequenceMotion(Node):
                 ],
                 orientation_constraints = [
                     OrientationConstraint(
-                        header = Header(frame_id = self.base),
+                        header = Header(frame_id = self._base),
                         link_name = self._end_effector,
                         orientation = target_pose.orientation,
                         absolute_x_axis_tolerance = 0.001,
@@ -83,6 +83,7 @@ def main(args=None):
     rclpy.init(args=args)
     sequence_motion = SequenceMotion()
     target_poses = [
+        # Pose(position = Point(x = 1.0, y = 1.0, z = 1.266), orientation = Quaternion(w = 1.0)),
         Pose(position = Point(x = 0.0, y = 0.0, z = 1.0), orientation = Quaternion(w = 1.0)),
         Pose(position = Point(x = 0.2, y = 0.2, z = 0.8), orientation = Quaternion(w = 1.0)),
         Pose(position = Point(x = -0.06, y = -0.32, z = 1.0), orientation = Quaternion(w = 1.0)),
