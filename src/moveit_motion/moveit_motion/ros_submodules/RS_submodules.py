39de9473fcdffb3b7adcea08050efb56e5fc9b3c
rishabh shukla <33500470+rysabh@users.noreply.github.com>
2024-10-01 04:14:21 +0530

    Update RS_submodules.py

--- src/moveit_motion/moveit_motion/ros_submodules/RS_submodules.py
+++ src/moveit_motion/moveit_motion/ros_submodules/RS_submodules.py
@@ -1,3 +1,22 @@
+# GNU GENERAL PUBLIC LICENSE
+# Version 3, 29 June 2007
+# 
+# Copyright (C) 2024 Rishabh Shukla
+# email: rysabh@gmail.com
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# see <https://www.gnu.org/licenses/>.
+# 
+# Written by Rishabh Shukla
+
 import os
 import pickle
 import csv
